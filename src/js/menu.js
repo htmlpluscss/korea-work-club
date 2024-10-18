e2dd9072152cac72be5170f9b28ec36e21698f97
mixkovrigin <159890280+mixkovrigin@users.noreply.github.com>
2024-10-18 13:27:45 +0300

    var

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,7 +3,7 @@
 	if ( btns.length > 0 ) {
 
 		window.addEventListener('click', event => {
-
+/*
 			if ( event.detail > 1 || ( event.target.closest('.menu__list') && event.target.closest('a') ) ) {
 
 				return;
@@ -15,7 +15,7 @@
 			[...btns].forEach( _btn => _btn.classList.toggle('is-open', btn !== null && _btn === btn && btn.classList.contains('is-open') === false ) );
 
 			document.body.classList.toggle('is-open-menu', [...btns].some( btn => btn.classList.contains('is-open') ) );
-
+*/
 		});
 
 	}
