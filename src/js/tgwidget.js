46b60079672480e389a98365454ef8c5f0e386af
mixkovrigin <159890280+mixkovrigin@users.noreply.github.com>
2024-10-19 11:56:23 +0300

    subscribe-telegram

---
+++ src/js/tgwidget.js
@@ -0,0 +1,60 @@
+( tgwidget => {
+
+	let fastLoadTimeout = true;
+
+	const init = () => {
+
+		fastLoadTimeout = null;
+
+		const iframe = document.createElement('iframe');
+
+		iframe.setAttribute('src', tgwidget.getAttribute('data-tgwidget'));
+
+		tgwidget.append(iframe);
+
+	}
+
+	const appendFrame = () => {
+
+		if ( fastLoadTimeout ) {
+
+			init();
+
+		}
+
+		window.removeEventListener('scroll',appendFrame);
+		window.removeEventListener('click',appendFrame);
+
+	}
+
+	const observer = new IntersectionObserver((entries, observer) => {
+
+		let isInViewport = false;
+
+		isInViewport = [...entries].some( entry => {
+
+			observer.unobserve(entry.target);
+
+			return entry.isIntersecting;
+
+		});
+
+		if ( isInViewport ) {
+
+			appendFrame();
+
+		}
+		else {
+
+			fastLoadTimeout = setTimeout( appendFrame, 30000 );
+
+		}
+
+	});
+
+	observer.observe(tgwidget);
+
+	window.addEventListener('scroll',appendFrame);
+	window.addEventListener('click',appendFrame);
+
+})(document.querySelector('[data-tgwidget]'));
